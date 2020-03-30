437d12c7016e9173dd5b75ba503e1886ae17fbfd
Asket Kaur <asketkaur97@gmail.com>
2020-03-30 16:12:31 -0600

    added Interaction

--- assets/js/social.js
+++ assets/js/social.js
@@ -1,24 +1,24 @@
 /*
  * Parse the data and create a graph with the data.
  */
-function parseData(createGraph) {
+function parseData(createGraph,x) {
 	Papa.parse("../data/37100072.csv", {
 		download: true,
 		complete: function(results) {
 			console.log(results.data);
-			createGraph(results.data);
+			createGraph(results.data,x);
 		}
 	});
 }
 
-function createGraph(data) {
+function createGraph(data, x) {
 
 	var years = [];
 	var females = ["Females"];
 	var males = ["Males"];
 
 	for (var i = 1; i<data.length; i++){
-		if((data[i][1] === "Canada") && (data[i][3] === "Total, registration status") && (data[i][4] === "Total, program level") && (data[i][5] === "Social and behavioural sciences and law")){
+		if((data[i][1] === "Canada") && (data[i][3] === "Total, registration status") && (data[i][4] === "Total, program level") && (data[i][5] === x)){
 		if (data[i][6] === "Females"){
 						years.push(data[i][0]);
 						females.push(data[i][13]);
@@ -51,7 +51,13 @@ function createGraph(data) {
 		      }
 		  }
 		});
-		
+
 }
 
-parseData(createGraph);
+function myFunction() {
+  var x = document.getElementById("fields").value;
+	console.log(x);
+	parseData(createGraph, x);
+}
+
+// parseData(createGraph);
--- assets/js/tech.js
+++ assets/js/tech.js
@@ -1,24 +1,28 @@
 /*
  * Parse the data and create a graph with the data.
  */
-function parseData(createGraph) {
-	Papa.parse("../data/37100072.csv", {
-		download: true,
-		complete: function(results) {
-			console.log(results.data);
-			createGraph(results.data);
-		}
-	});
-}
+// var e = document.getElementById("fields");
+ // var strUser = e.options[e.selectedIndex].value;
+ function parseData(createGraph, value, id) {
+	 console.log("hello")
+ 	Papa.parse("../data/37100072.csv", {
+ 		download: true,
+ 		complete: function(results) {
+ 			console.log(results.data);
+ 			createGraph(results.data, value, id);
+ 		}
+ 	});
+ }
 
-function createGraph(data) {
+
+function createGraph(data, value, id) {
 
 	var years = [];
 	var females = ["Females"];
 	var males = ["Males"];
 
 	for (var i = 1; i<data.length; i++){
-		if((data[i][1] === "Canada") && (data[i][3] === "Total, registration status") && (data[i][4] === "Total, program level") && (data[i][5] === "Mathematics, computer and information sciences")){
+		if((data[i][1] === "Canada") && (data[i][3] === "Total, registration status") && (data[i][4] === "Total, program level") && (data[i][5] === value)){
 		if (data[i][6] === "Females"){
 						years.push(data[i][0]);
 						females.push(data[i][13]);
@@ -28,10 +32,10 @@ function createGraph(data) {
 		}
 	}
 
-	console.log(females);
+	// console.log(females);
 
 	var chart = c3.generate({
-			bindto: '#tech',
+			bindto: document.getElementById(id),
 		  data: {
 		      columns: [
 							males,
@@ -53,4 +57,11 @@ function createGraph(data) {
 		});
 }
 
-parseData(createGraph);
+// parseData(createGraph, document.getElementById("fields").value, "tech" );
+
+
+function myFunction() {
+  var x = document.getElementById("fields").value;
+	console.log(x);
+	parseData(createGraph, x, "edu");
+}
